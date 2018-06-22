16acdf784c576e21f5233c2d3a3c841ed6218589
Unknown <mrtmartis@yahoo.co.uk>
2018-06-22 10:12:34 +0300

    added product functionality

--- resources/views/pages/about.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>This is About page</h1>
-@endsection
--- resources/views/pages/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>This is Index page {{$title}}</h1>
-@endsection
